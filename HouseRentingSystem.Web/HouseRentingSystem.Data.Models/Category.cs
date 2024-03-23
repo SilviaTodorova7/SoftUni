ef5b61173e4e832ccdd3c84b57ac6b0a218af762
Silvia Todorova <164194780+SilviaTodorova7@users.noreply.github.com>
2024-03-23 18:53:35 +0200

    ModelsAded
    ModelsAded

---
+++ HouseRentingSystem.Web/HouseRentingSystem.Data.Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Common.EntityValidationConstants.Category;
+
+namespace HouseRentingSystem.Data.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+    }
+}
